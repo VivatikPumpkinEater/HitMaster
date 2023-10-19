af05d12bd57f0e176bc3045ae67dedda1e15e487
Ivan Rusak <vivatik.pumpkin.eater@gmail.com>
2023-10-19 19:47:05 +0300

    ...

--- Assets/Content/Scripts/Controllers/LevelProgressController.cs
+++ Assets/Content/Scripts/Controllers/LevelProgressController.cs
@@ -1,5 +1,4 @@
 ﻿using Cysharp.Threading.Tasks;
-using UnityEngine;
 using UnityEngine.AddressableAssets;
 
 public class LevelProgressController
--- Assets/Content/Scripts/Location/Data/LocationStaticData.cs
+++ Assets/Content/Scripts/Location/Data/LocationStaticData.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-/// <summary> Статические постоянные данные объекта. Не надо хранить в прогрессе </summary>
+/// <summary> Статические постоянные данные объекта.</summary>
 [Serializable]
 public class LocationStaticData
 {
